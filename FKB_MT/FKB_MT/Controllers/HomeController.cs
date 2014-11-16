c04120188ff62c49ef2272b2dc953e3657107c9a
dziewczyna-informatyka <martawolkowska@gmail.com>
2014-11-16 20:41:35 +0100

    preliminary layout fo page

--- FKB_MT/FKB_MT/Controllers/HomeController.cs
+++ FKB_MT/FKB_MT/Controllers/HomeController.cs
@@ -26,5 +26,47 @@ namespace FKB_MT.Controllers
 
             return View();
         }
+
+        public ActionResult MachineDetails()
+        {
+            ViewBag.Message = "Main page with list of machine details.";
+
+            return View();
+        }
+
+        public ActionResult Repairs()
+        {
+            ViewBag.Message = "Here will be page with repairs";
+
+            return View();
+        }
+
+        public ActionResult Adjustments()
+        {
+            ViewBag.Message = "Here will be pagw with adjustments";
+
+            return View();
+        }
+
+        public ActionResult Inspections()
+        {
+            ViewBag.Message = "Here will be page with inspections";
+
+            return View();
+        }
+
+        public ActionResult Calibrations()
+        {
+            ViewBag.Message = "Here will be page with calibrations";
+
+            return View();
+        }
+
+        public ActionResult Validations()
+        {
+            ViewBag.Message = "Here will be page with validations";
+
+            return View();
+        }
     }
 }
