c29e037de14a0acf6b835699f6f0fd338e2b59f0
dziewczyna-informatyka <martawolkowska@gmail.com>
2014-11-16 14:04:00 +0100

    first commit

---
+++ FKB_MT/FKB_MT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FKB_MT.Startup))]
+namespace FKB_MT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
